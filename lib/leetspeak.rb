4e5c6630d09709070bc0fea93c2421dfcabf5947
Alexander Jacks and James Turello <alexander@>
2016-06-27 13:07:51 -0700

    add additional specs

--- lib/leetspeak.rb
+++ lib/leetspeak.rb
@@ -2,6 +2,8 @@ class String
   define_method(:leetspeak) do
     self.gsub!("e","3")
     self.gsub!("o","0")
+    self.gsub!("I","1")
+    self[1..self.length-1].gsub!("s","z")
     self
   end
 end
--- spec/leetspeak_spec.rb
+++ spec/leetspeak_spec.rb
@@ -10,9 +10,21 @@ describe('String#leetspeak') do
   it('replaces every "e" in a string with a "3"') do
     expect("enter".leetspeak).to(eq("3nt3r"))
   end
-  
+
   it('replaces every "o" in a string with a "0"') do
-     expect("boo boo".leetspeak).to(eq("b00 b00"))
-   end
+    expect("boo boo".leetspeak).to(eq("b00 b00"))
+  end
+
+  it('replaces every "I" in a string with a "1"') do
+    expect("I like Ike".leetspeak).to(eq("1 lik3 1k3"))
+  end
+
+  it('replaces every "s" in a string with a "z"') do
+    expect("roses".leetspeak).to(eq("r0z3z"))
+  end
+
+  it('does NOT replace the first letter when it is an s') do
+    expect("sassafrass".leetspeak).to(eq("sazzafrazz"))
+  end
 
 end
