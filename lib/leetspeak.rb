fe0bc1833cf54fc83a7cdf148ec8a20df9556825
Alexander Jacks and James Turello <alexander@>
2016-06-27 14:00:40 -0700

    adds successful test condition for input with first letter as S

--- lib/leetspeak.rb
+++ lib/leetspeak.rb
@@ -3,7 +3,14 @@ class String
     self.gsub!("e","3")
     self.gsub!("o","0")
     self.gsub!("I","1")
-    self[1..self.length-1].gsub!("s","z")
+    if self[0] == "s"
+      word = self[1..self.length - 1]
+      word.gsub!("s","z")
+      return "s" + word
+      puts "s" + word
+    else
+      self.gsub!("s","z")
+    end
     self
   end
 end
